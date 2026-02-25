e8825089bd9ef9fd741967be0eea2d54234de2d7
juan200gt <jjimgon0107@g.educaand.es>
2026-02-25 20:18:14 +0100

    feat(pages): añade HomePage como componente contenedor e implementa enrutado base

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home/home.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent,
+        title: 'OutfitGo - Inicio'
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
+
