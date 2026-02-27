5aed0c8c14530bc6210978f10d317ac1f3f2118a
juan200gt <jjimgon0107@g.educaand.es>
2026-02-27 18:58:07 +0100

    feat(pages): implementa página de login inteligente y su enrutado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomePageComponent,
-        title: 'OutfitGo - Inicio'
+        title: 'Inicio'
+    },
+    {
+        path: 'login',
+        component: LoginPageComponent,
+        title: 'Iniciar Sesión'
     },
     {
         path: '**',
---
+++ src/app/pages/login-page/login-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, inject, signal } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
+import { LoginCredentials } from '../../interfaces/auth';
+
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [LoginFormComponent],
+  templateUrl: './login-page.component.html',
+})
+export class LoginPageComponent {
+  #router = inject(Router);
+
+  loading = signal<boolean>(false);
+  error = signal<string | null>(null);
+
+  onLogin(credentials: LoginCredentials) {
+    this.loading.set(true);
+    this.error.set(null); // Limpia posibles errores previos
+
+    // Simulación de llamada a backend
+    setTimeout(() => {
+      this.loading.set(false);
+      this.error.set(null);
+      this.#router.navigate(['/products']);
+    }, 2000);
+  }
+}
