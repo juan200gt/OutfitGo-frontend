a1388f5d56ee1b6ea0927f7597eddb4ba4f3cb7e
Luis Godino Wallace <lgodwal2312@g.educaand.es>
2026-04-22 12:28:56 +0200

    Mover boton de contacto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, signal, inject, computed } from '@angular/core';
 import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { TranslateModule } from '@ngx-translate/core';
 import { CartService } from './services/cart.service';
 import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, RouterLink, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
